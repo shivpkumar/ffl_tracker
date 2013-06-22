7cd9b3d7a2c5d7ce7b30e6c67c9b1ca10cc5a939
Apprentice <apprentice@dbc01.local>
2013-06-21 17:19:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FflTracker::Application.config.secret_token = 'b17704c0c24162d5b4b9053ef57dfb7c3ce98a5f3d33f4d33169d05ad59c952ac396009988672c1db37bc184b9a3f6ce324969b1535af233350fde097e8f3417'
